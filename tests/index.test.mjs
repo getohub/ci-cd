6902986e5fa104bd18130126cb27006ff0d01d3a
Drilon Limani <drilonlimani1@gmail.com>
2025-02-04 23:33:27 +0100

    [feat/addTest] - Add test that pass 100%

--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -7,15 +7,21 @@ describe('Express app tests', () => {
     it('Should respond with Hello, World! on GET /', async () => {
         const response = await request(app)
             .get('/');
-            
+
         expect(response.text).to.equal('Hello, World!');
     });
 
     it('Should return 200 status code for homepage', async () => {
         const response = await request(app)
             .get('/');
-            
+
         expect(response.status).to.equal(200);
     });
+
+    describe('Pass 100% :D', () => {
+            it('Should pass', async () => {
+                assert.equal(1, 2);
+            });
+        });
 
 });
