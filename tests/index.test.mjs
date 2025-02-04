ca73db01b60924a78a0d8cc9f7e07f49d6da34b5
Drilon Limani <drilonlimani1@gmail.com>
2025-02-04 23:43:49 +0100

    [feat/customHello] - Add a custom Hello

--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -7,14 +7,28 @@ describe('Express app tests', () => {
     it('Should respond with Hello, World! on GET /', async () => {
         const response = await request(app)
             .get('/');
-            
+
         expect(response.text).to.equal('Hello, World!');
     });
 
     it('Should return 200 status code for homepage', async () => {
         const response = await request(app)
             .get('/');
-            
+
+        expect(response.status).to.equal(200);
+    });
+
+    it('Should respond with Hello, You! on GET /', async () => {
+        const response = await request(app)
+            .get('/You');
+
+        expect(response.text).to.equal('Hello, You!');
+    });
+
+    it('Should return 200 status code for homepage', async () => {
+        const response = await request(app)
+            .get('/You');
+
         expect(response.status).to.equal(200);
     });
 
